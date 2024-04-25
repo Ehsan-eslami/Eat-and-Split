732807bfc984008958d2bef8419429d597f3c876
EHSAN\esehs <eslami.ehsan200@gmail.com>
2024-04-25 16:06:12 +0330

    "Friend component: updated layout and conditional rendering of balance text"
    This commit includes changes to the Friend component in the file Friend.js. The layout of the component has been updated to wrap the list item and balance text in a containing

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -7,28 +7,32 @@ function Friend({ friend, onSelection, selectedFriends }) {
   const isSelected = selectedFriends?.id === friend?.id;
 
   return (
-    <li className={isSelected ? "selected relative" : ""}>
-      <img src={friend.image} alt={friend.name} />
-      <h3>{friend.name}</h3>
+    <>
+      <div className=' relative'>
+        <li className={isSelected ? "selected" : ""}>
+          <img src={friend.image} alt={friend.name} />
+          <h3>{friend.name}</h3>
 
-      {friend.balance < 0 && (
-        <p className="red">
-          You owe {friend.name} {Math.abs(friend.balance)}$
-        </p>
-      )}
-      {friend.balance > 0 && (
-        <p className="green">
-          {friend.name} owes you {Math.abs(friend.balance)}$
-        </p>
-      )}
-      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+          {friend.balance < 0 && (
+            <p className="red">
+              You owe {friend.name} {Math.abs(friend.balance)}$
+            </p>
+          )}
+          {friend.balance > 0 && (
+            <p className="green">
+              {friend.name} owes you {Math.abs(friend.balance)}$
+            </p>
+          )}
+          {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <Button onClick={() => onSelection(friend)}>
-        {isSelected ? "Close" : "Select"}
-      </Button>
-      <div className=' absolute top-[-140px] right-0'>
+          <Button onClick={() => onSelection(friend)}>
+            {isSelected ? "Close" : "Select"}
+          </Button>
+        </li>
+        <div className=' absolute top-[-140px] right-0'>
           <img src="/Svgs/fun-moments.svg" alt="" />
         </div>
-    </li>
+      </div>
+    </>
   );
 }
