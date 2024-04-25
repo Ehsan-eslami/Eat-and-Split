16c8c735e1f8f8112d97c9e1c4b5e163ceccefe5
EHSAN\esehs <eslami.ehsan200@gmail.com>
2024-04-25 14:16:41 +0330

    Update LoadingForm component design and add media queries to index.css for responsiveness
    The LoadingForm component's design has been updated with a new background color and revised heights for some elements. Additionally, media queries have been

--- src/components/LoadingForm.js
+++ src/components/LoadingForm.js
@@ -3,15 +3,15 @@ import React from 'react';
 export default function LoadingForm({ friends }) {
   return (
     <>
-      <div className="border border-[#ff922b]  shadow rounded-xl p-4  w-full mx-auto">
+      <div className="border  bg-[#ffe8cc] shadow rounded-xl p-4  w-full mx-auto">
         <div className="animate-pulse flex space-x-4">
           <div className="rounded-full bg-[#4b4a49] h-14 w-14 my-auto"></div>
-          <div className="flex-1 space-y-3 py-1">
+          <div className="flex-1 space-y-5 py-1">
             <div className="grid grid-cols-3 gap-4">
-              <div className="h-6 bg-[#4b4a49] rounded col-span-2"></div>
-              <div className="h-6 bg-[#d8a267] rounded col-span-1 "></div>
+              <div className="h-4 bg-[#4b4a49] rounded col-span-2"></div>
+              <div className="h-4 bg-[#d8a267] rounded col-span-1 "></div>
             </div>
-            <div className="h-6 bg-[#4b4a49] rounded"></div>
+            <div className="h-4 bg-[#4b4a49] rounded"></div>
           </div>
         </div>
       </div>
